292a6da71387e03c2e39e44a30761218af0f45a9
dmitri <diman896@gmail.com>
2017-03-26 02:29:42 +0300

    renderer

--- UI/Renderer.cs
+++ UI/Renderer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Shapes;
 
 namespace UI
@@ -16,11 +17,13 @@ namespace UI
             double cell_size = 50;
 
             for (int i = 0; i < cell_number; i++)
+                for (int j = 0; j < cell_number; j++)
             {
                 Rectangle r = new Rectangle();
                 r.Width = cell_size;
                 r.Height = cell_size;
-                r.Stroke = 
+                r.Stroke = Brushes.Black;
+                    r.Margin = new System.Windows.Thickness(i * cell_size, j * cell_size);
             }
         }
     }
